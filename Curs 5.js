ff0f0b246e592611d14a938c707e43e19073b12a
Soare Florin <fsoare90@gmail.com>
2022-04-08 23:25:18 +0300

    08042022

--- Curs 5.js
+++ Curs 5.js
@@ -1,87 +1,107 @@
-let numar =1; //definesc variabila numar si asignez valoarea 1 (comentariu pe o linie)
-let numarCuVirgula =1.5;
-let sirDeCaractere ="Ana are mere"; 
-let sirDeCaractere2 ='Ana are mere !!!';
+let t = undefined; // atunci cand nu am asignat nimic unei variabile
+let t2 = null; // cand vreau ca variabila sa nu contina nimic
+
+let numar = 1; //definesc variabila numar si asignez valoarea 1
+let numarCuVirgula = 1.5;
+let sirDeCaractere = "Ana Are mere";
+let sirDeCaractere2 = "Ana Are mere !!!";
 let boolTrue = true;
 let boolFalse = false;
-/*
-cometariu
-pe mai 
-multe linii
-*/
+
 //matematica
 numar = 1 + 2;
 numar = numar * 2;
-numarCuVirgula = 1 + (2 * 3) - 7;
-numar *= 2; //inseamna numar = numar * 2;
-numar += 2; //inseamna numar = numar + 2;
-numar -= 2; //inseamna numar = numar - 2;
-numar /= 2; //inseamna numar = numar / 2;
-numar++; // inseamna numar= numar + 1
-numar--; // inseamna numar= numar - 1
-numar = numar%2; // restul impartiri; daca da 0 este par daca da 1 este impar;
-numar = numar**; //ridicare la putere;
-numar= Math.pow(numar,3)// inseamna numar la puterea 3;
-//sa tin minte string(sir de caractere)
+numarCuVirgula = 1 + 2 * 3 - 7;
+numar *= 2; // inseamna numar = numar * 2;
+numar += 2; // inseamna numar = numar + 2;
+numar -= 2; // inseamna numar = numar - 2;
+numar /= 2; // inseamna numar = numar / 2;
+numar++; // inseamna numar = numar +1
+numar--; // inseamna numar = numar -1
+numar = numar % 2; // restul impartirii;
+numar = numar ** 3; // inseamna numar la puterea 3
+numar = Math.pow(numar, 3); // inseamna numar la puterea 3
 
-let sir = "A" + "B"; //un nou sir de caractere "AB"
-sir +="Mergeee";
-//nu se pot scadea numar de caractere si nu putem aduna numere cu caractere (se poate dar nu se recomanda)
-//Exemplu de caracter+cifra.
-let nuFaceAsa= 3+"A" // "3A"
-let nuFaceAsta2= 2+3+"A" // "5A"
-let nuFaceAsta3= "B" + 2 + 3 +"A"; // "B23A" daca pun parenteze la 2 si 3 atunci da B5A;
-String(23) // transforma in sir de caractere ex:"23"
+//sir de caractere (string)
+let sir = "A" + "B"; // un nou sir de caractere "AB"
+sir += "Mergeee";
 
+let nuFaceAsta = 3 + "A"; // "3A"
+let nuFaceAsta2 = 2 + 3 + "A"; // "5A"
+let nuFaceAsta3 = "b" + 2 + 3 + "A"; // "b23A"
 
-//Facem boolean 
-// Exista doar treu(1) si false(0) 
-// !(not)-> !true = false
-// !(not)-> !false = true
+//boolean
+//exista true (1) si false (0)
 
-// AND (in cod se scrie asa "&&") inmultire
+//!true => false
+//!false => true
+
+//AND && inmultire
 let x = true && false;
-// OR (in cod se scrie asa "||") adunare
-let x = true || false;
+
+//OR || adunare
+let y = true || false;
 
 if (conditia) {
-    //in cazul in care conditia este adevarata 
-}
-else{
-    //in cazul in care conditia este falsa.
+  //in cazul in care conditia este adevarata
+} else {
+  //in cazul in care conditia este falsa
 }
 
-//=======================================================================================================
+//=============================================================
+let ghiciNumarul = 5;
+let numarIncercari = 3;
+let xx = "5";
 
-let ghiciNumarul= 5;
-let numarIncercari= 3;
+let yy = xx + ghiciNumarul;
 
 if (ghiciNumarul === 6) {
-    numarIncercari--;
-    ghiciNumarul++;
-} 
-else {
-    alert ("Bravo ai ghicit");
-}
-
-// egalitate  ===
-// diferit !==
-// mai mare >
-// mai mic <
-// mai mic sau egal <=
-// mai mare sau egal >=
-// negare ! 
-
-if(ghiciNumarul1 % 2 === 0){
-    alert( "Numarul " ghiciNumarul1 + "este par");// Numarul 5 este par
-    
+  numarIncercari--;
 } else {
-    alert( `Numarul ${ghiciNumarul1} este impar`);
+  alert("Bravo ai ghicit");
 }
 
-"ana are \"3\" mare" //Ana are 3 mere 
+//egalitate === (strict)
+//diferit !== (strict)
+//mai mare >
+//mai mic <
+//mai mic sau egal <=
+//mai mare sau egal >=
+//negare !
 
+if (ghiciNumarul % 3 === 2) {
+  alert("Numarul " + ghiciNumarul + " este par"); //Numarul 5 este par
+} else if (ghiciNumarul % 3 === 1) {
+  alert("Numarul " + ghiciNumarul + " este par"); //Numarul 5 este par
+} else {
+  alert(`Numarul ${ghiciNumarul} este impar`);
+}
 
+function clatite(parametrii) {
+  //sparg oua
+  //pun faina
+  //le bat
+  //prajesc in tigaie
 
-// ========================================================================================================================================================================             Curs 6 
+  return clatita;
+}
 
+function par(nr) {
+  if (nr % 2 === 0) {
+    return "par";
+  } else {
+    return "impar";
+  }
+}
+
+function par(nr) {
+  let rezultat;
+  if (nr % 2 === 0) {
+    rezultat = "par";
+  } else {
+    rezultat = "impar";
+  }
+  return rezultat;
+}
+
+let u = par(5);
